be48eaff09ed699654d1f5fe56a3d0c27d2f68e8
Jeongseop Lee <wanderkind@semyung.ac.kr>
2022-10-23 14:46:07 +0900

    Create A-Batting Average.py

---
+++ AtCoder/キーエンスプログラミングコンテスト2022（AtCoder Beginner Contest 274）/A-Batting Average.py
@@ -0,0 +1,14 @@
+from math import *
+import sys
+input=lambda:sys.stdin.readline().strip()
+U=lambda:map(int,input().split())
+
+'''
+def solve():
+    return 0
+'''
+
+
+a,b=U()
+z=b/a
+print(f'{z:.3f}')

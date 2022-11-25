cb9cb72b97f889cb5357d2ce0ae5d152c702aed1
Jeongseop Lee <wanderkind@semyung.ac.kr>
2022-11-26 03:11:57 +0900

    Create A-SSeeeeiinngg DDoouubbllee.py

---
+++ Codeforces/Codeforces Round #836 (Div. 2)/A-SSeeeeiinngg DDoouubbllee.py
@@ -0,0 +1,21 @@
+
+
+from math import *
+import sys
+input=lambda:sys.stdin.readline().strip()
+U=lambda:map(int,input().split())
+
+
+def sol():
+    s=sorted(list(input()))
+    print(''.join(s+s[::-1]))
+    
+    
+  
+    
+
+for _ in range(int(input())):
+    
+    sol()
+    
+    print()

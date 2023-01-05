462457cd2d4b3c90bb139a735c791398910f0118
Jeongseop Lee <wanderkind@semyung.ac.kr>
2023-01-06 02:21:42 +0900

    Create A-Greatest Convex.py

---
+++ Codeforces/Codeforces Round #842 (Div. 2)/A-Greatest Convex.py
@@ -0,0 +1,14 @@
+
+from math import *
+import sys
+input=lambda:sys.stdin.readline().strip()
+U=lambda:map(int,input().split())
+
+def sol():
+    k=int(input())
+    
+    print(k-1)
+
+for _ in range(int(input())):
+    sol()
+    print()
